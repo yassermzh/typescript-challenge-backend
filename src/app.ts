5ec8efae2044399d14da42c79f0de0933c99c1d5
Yasser Zadeh <yassermzh.germany@gmail.com>
2021-11-16 01:45:37 +0100

    issue 3 - add cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import { json } from 'body-parser'
 import * as express from 'express'
 import * as morgan from 'morgan'
+import * as cors from 'cors'
 import { transitLinesRouter } from 'src/api/transit-lines'
 
 export const app = express()
 
 app.use(morgan('dev'))
-
+app.use(cors())
 app.use(json())
 
 app.use('/transit-lines', transitLinesRouter)
